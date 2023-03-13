65ba13e09e27f034c41a6b8232d0268c9716cae8
bczhc <bczhc0@126.com>
2023-03-13 18:36:19 +0800

    card-balance: get json data via `page.on('response')'`

--- card-balance.js
+++ card-balance.js
@@ -16,7 +16,7 @@ let url = 'http://ehall.ccit.js.cn/ywtb-portal/standard/index.html?browser=no#/h
 async function launchBrowser() {
     return await puppeteer.launch({
         executablePath: '/usr/bin/chromium',
-        headless: true,
+        headless: false,
         args: ['--no-proxy-server', '--disable-gpu', '--no-sandbox']
     });
 }
@@ -56,14 +56,18 @@ let browser = await launchBrowser();
 let page = await loadPage(browser);
 await login(page);
 
-await page.waitForSelector('div[title="点击查看消费流水>>"]');
-await page.waitForNetworkIdle();
-
-let balance = await page.evaluate(() => {
-    let r = /账户余额为:([0-9]*\.[0-9]*)元/;
-    return Array.from(document.querySelectorAll('div')).map(x => x.title).filter(x => r.test(x))[0].match(r)[1];
+page.on('response', async response => {
+    if (response.url().includes('getViewDataDetail')) {
+        let json = await response.json();
+        console.error("Received response");
+        try {
+            let r = /账户余额为:([0-9]*\.[0-9]*)元/;
+            let subtitle = json['data'][0]['subTitle'];
+            if (r.test(subtitle)) {
+                console.log(subtitle.match(r)[1]);
+                await browser.close();
+            }
+        } catch (ignored) {}
+    }
 });
 
-await browser.close();
-
-console.log(balance);
